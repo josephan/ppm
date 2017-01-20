2b1ae71692737f3cc48649d1ac5de43a70cf1567
Joseph An <an.joseph891@gmail.com>
2017-01-19 20:15:57 -0500

    validations for models

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,6 @@ class Game < ActiveRecord::Base
   has_many :players
   has_many :users, through: :players
   accepts_nested_attributes_for :players
+
+  validates :date_played, presence: true
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,7 @@
 class Player < ActiveRecord::Base
   belongs_to :user
   belongs_to :game
+
+  validates :user, presence: true
+  validates :score, numericality: true, inclusion: 0..21
 end
